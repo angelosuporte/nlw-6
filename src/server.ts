8354f17c617084f71b63917cb1099ab2060e3965
Angelo Carvalho <angelosuporteti@gmail.com>
2021-06-22 12:06:52 -0300

    Get and POST routers

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,13 @@ import express from "express";
 // @types/express
 const app = express();
 
+app.get("/test", (request, response) => {
+    return response.send("Hello World!")
+});
+
+app.post("/test-post", (request, response) =>{
+    return response.send("--------->Post");
+});
+
 // http://localhost:3000
 app.listen(3000, () => console.log ("Server is running"));
