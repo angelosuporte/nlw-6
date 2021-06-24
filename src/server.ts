36fb48d69b60bc30b46ca155791733bf96375254
Angelo Carvalho <angelosuporteti@gmail.com>
2021-06-23 23:38:11 -0300

    CreateUserController

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import express from "express";
 
+import { router } from "./routes";
+
 import "./database";
 
-// @types/express
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Hello World!")
-});
+app.use(express.json());
 
-app.post("/test-post", (request, response) =>{
-    return response.send("--------->Post");
-});
+app.use(router);
 
 // http://localhost:3000
 app.listen(3000, () => console.log ("Server is running"));
